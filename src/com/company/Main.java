dbe144661c59052c8f5a3c81086161f827c1e5f6
Aleksandr Masleev <masleev2018@yandex.ru>
2021-01-29 01:06:01 +0300

    create my first program in IntelliJ IDEA

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int a = 1;
+        int b = 2;
+        System.out.println("a + b = " + (a+b));
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+
+    }
+}
